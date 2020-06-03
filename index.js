71b60afea2c392554119a79f20c561f0b1f3f51e
skimalee <skimalee@gmail.com>
2020-06-03 15:01:38 -0700

    Video Duration is shown in console

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg')
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({
         webPreferences: {
-        nodeIntegration: true
+            nodeIntegration: true
         }
     });
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-    
+    mainWindow.loadURL(`file://${__dirname}/index.html`);  
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log('Video duration is:', metadata.format.duration);
+    })
 });
